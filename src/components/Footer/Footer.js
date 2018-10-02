1656b87d5fc71a9ebadb48f679924830c5acc02d
Ian Banson <Jayredd11@gmail.com>
2018-10-02 11:41:05 -0400

    first

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+//sets up the reusable Footer component
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<header className="footer">
+		<p>Good Luck!</p>
+	</header>
+);
+
+export default Footer;
